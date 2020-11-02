146afc136571c91dfa04fba0de92f950e62fab15
Preetham869 <70512182+Preetham869@users.noreply.github.com>
2020-11-02 18:35:24 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,26 @@
+class Plinko {
+    constructor(x, y,r) {
+      var options = {
+        'isStatic': false,
+        'restitution':0.3,
+        'friction':1.5,
+        'density':1.2
+      }
+      this.x = x
+      this.y = y
+      this.r = r
+      this.body = Bodies.circle(x, y,this.r,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos = this.body.position;
+      push();
+      translate(pos.x,pos.y);
+      fill("brown")
+      ellipse(this.x, this.y,this.r);
+      
+      pop();
+    
+    }
+  };
+  
